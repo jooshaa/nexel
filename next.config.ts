6fcfb47c66eade560f367000ab158014af2edee9
Josha <kutmonbekovjoshqin0@gmail.com>
2026-05-14 15:41:59 +0500

    deployment

--- next.config.ts
+++ next.config.ts
@@ -18,9 +18,8 @@ const nextConfig: NextConfig = {
         pathname: '/uploads/**',
       },
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '1337',
+        protocol: 'https',
+        hostname: 'nexel-strapi.etamin.uz',
         pathname: '/uploads/**',
       },
     ],
